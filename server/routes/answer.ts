f83267759d892abe6d906a4058c5e478eed8a927
CarlyyyChen <huanyuechen@outlook.com>
2025-04-10 20:44:39 -0400

    refactor answer page to answer route and controller. Merge openAPI yaml files.

--- server/routes/answer.ts
+++ server/routes/answer.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { addAnswer } from "../controllers/answerController";
+import { addAnswer, voteAnswer } from "../controllers/answerController";
 
 const router = express.Router();
 
 router.post("/addAnswer", addAnswer);
+router.patch("/:aid/vote", voteAnswer);
 
 export default router;
